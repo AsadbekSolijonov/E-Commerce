d294d3fd22ca7f40521edf792b2d0b10815b07f3
Asadbek <>
2022-07-17 21:52:04 +0500

    Initial commit

---
+++ store/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Model(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='store/images')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
