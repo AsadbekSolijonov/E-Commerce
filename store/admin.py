73da16e746e3b45967d3179f573842aefb3cdc8f
Asadbek <>
2022-07-18 11:07:40 +0500

    data structure

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from store.models import Customer, Order, OrderItem, ShippingAdress, Product
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAdress)
+admin.site.register(Product)
